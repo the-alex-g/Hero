0103999335de7086cb2864de1004460b064da4f3
Alex <alex.gestwicki@gmail.com>
2021-04-10 10:11:56 -0400

    Integrate attack animations

--- project/Player/Player.gd
+++ project/Player/Player.gd
@@ -6,7 +6,7 @@ extends KinematicBody2D
 const UP_VECTOR := Vector2.UP
 
 # enums
-enum State {WALKING, IDLE}
+enum State {WALKING, IDLE, AIRBORNE}
 
 # exported variables
 export var speed := 200
@@ -16,7 +16,6 @@ export var jump_strength := 200
 export var attack_cooldown_time := 0.5
 
 # normal variables
-var color := Color.white
 var _ignore
 var _player_index := "1"
 var _action_key := "player_"
@@ -28,6 +27,7 @@ var _can_jump := true
 var _can_attack := true
 var _smash_attack := false
 var _attacking := false
+var _was_attacking := false
 
 # onready variables
 onready var collision := $CollisionShape2D
@@ -38,7 +38,7 @@ onready var _body := $Body
 
 
 func _ready()->void:
-	_animation_tree.active = false
+	_animation_tree.active = true
 	_action_key += _player_index + "_"
 
 
@@ -77,34 +77,55 @@ func _process(delta:float)->void:
 		_body.scale.x = 0.5 if velocity.x > 0 else -0.5
 		_state = State.WALKING
 		velocity.x *= speed
-	else:
+	elif velocity.x == 0:
 		_state = State.IDLE
+	if not is_on_floor():
+		_state = State.AIRBORNE
 	_ignore = move_and_slide(velocity, UP_VECTOR)
 	_get_animation()
 
 
 func _get_animation()->void:
+	# values from 0 (not hitting) to 1 (hitting)
+	var walk_hit := 0.0
+	var jump_hit := 0.0
+	var idle_hit := 0.0
+	
+	# values from -1 (walking) through 0 (jumping) to 1 (idle)
+	var master_action := 0.0
+	
 	if _state == State.WALKING:
-		if _attacking:
-			_animation_tree.active = true
-			_animations.play("Hit")
-			yield(get_tree().create_timer(0.2), "timeout")
-			_animation_tree.active = false
-			_attacking = false
-		_animations.play("Walk")
+		master_action = -1.0
+	
 	elif _state == State.IDLE:
-		if _attacking:
-			_animations.play("Hit")
-			yield(get_tree().create_timer(0.2), "timeout")
-			_attacking = false
-		else:
-			_animations.play("Idle")
+		master_action = 1.0
+	
+	_set_animation(idle_hit, jump_hit, walk_hit, master_action)
+
+
+func _set_animation(idle:float, jump:float, walk:float, master_value:float)->void:
+	# master is -1: walk, 0: jump, 1: idle
+	# the others are 0: base, 1: hit
+	if _attacking:
+		idle = 1.0
+		jump = 1.0
+		walk = 1.0
+		if not _was_attacking:
+			_animation_tree.set("parameters/IdleHitSeek/seek_position", 0)
+			_animation_tree.set("parameters/JumpHitSeek/seek_position", 0)
+			_animation_tree.set("parameters/WalkHitSeek/seek_position", 0)
+			_was_attacking = true
+	_animation_tree.set("parameters/WalkHit/add_amount", walk)
+	_animation_tree.set("parameters/JumpHit/add_amount", jump)
+	_animation_tree.set("parameters/IdleHit/add_amount", idle)
+	_animation_tree.set("parameters/Master/add_amount", master_value)
 
 
 func _attack():
 	_can_attack = false
 	_attacking = true
 	_attack_cooldown_timer.start(attack_cooldown_time)
+	$AttackAnimTimer.start(0.2)
 
 
 func _calculate_gravity(delta:float)->float:
@@ -113,5 +134,17 @@ func _calculate_gravity(delta:float)->float:
 	return lerp(0.0, max_gravity, _time_off_ground)
 
 
-func _on_AttackCooldownTimer_timeout():
+func _on_AttackCooldownTimer_timeout()->void:
 	_can_attack = true
+
+
+func _on_AttackAnimTimer_timeout()->void:
+	_attacking = false
+	_was_attacking = false
+	_stop_attacking_animations()
+
+
+func _stop_attacking_animations()->void:
+	_animation_tree.set("parameters/WalkHit/add_amount", 0.0)
+	_animation_tree.set("parameters/JumpHit/add_amount", 0.0)
+	_animation_tree.set("parameters/IdleHit/add_amount", 0.0)
